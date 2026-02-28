8565f40e357c69cce92ae2d75c00035e12754153
pinchase <kagiripeterson8404@gmail.com>
2026-02-28 15:43:04 +0300

    few

--- unipeer/settings.py
+++ unipeer/settings.py
@@ -141,10 +141,9 @@ REST_FRAMEWORK = {
 # -----------------------------
 # CORS
 # -----------------------------
-CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
-
-if DEBUG and not CORS_ALLOWED_ORIGINS:
-    CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://unipeer-frontend.vercel.app",
+]
 
 # -----------------------------
 # SECURITY (Production Hardened)
